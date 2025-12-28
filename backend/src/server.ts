56eca0637b488395c3325052b432d8e4527e14a7
yosrOmrane <141227751+yosrOmrane@users.noreply.github.com>
2025-12-28 16:11:58 +0100

    serv

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ import analyticsRoutes from './routes/analytics';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 // Middleware
 app.use(cors());
